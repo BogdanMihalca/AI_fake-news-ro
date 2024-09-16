6a1d09872e17bf76517fa333019b94d052a73a6e
Bogdan Mihalca <mihalcabogdan8@gmail.com>
2024-09-16 16:44:18 +0300

    attempt to reduce size

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,6 @@
-import { NextRequest } from "next/server";
-import authConfig from "./auth.config";
-import NextAuth from "next-auth";
 export { auth as middleware } from "@/auth";
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-const { auth } = NextAuth(authConfig);
-export default auth(async function middleware(req: NextRequest) {
-  // Your custom middleware logic goes here
-});
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
